630365cbd827bdd5ebefb5f59dbe400257f37833
Nwaokocha Victor <nvonweb@outlook.com>
2020-12-29 22:26:56 -0500

    implement search query resolver

--- server/modules/merchant/resolvers.js
+++ server/modules/merchant/resolvers.js
@@ -1,8 +1,40 @@
+const Fuse = require('fuse.js')
 const { merchants } = require("../../../mockMerchantData")
 
 const resolvers = {
   Query: {
-    merchants: () => merchants
+    merchants: () => merchants,
+    search: (parent, args) => {
+      const { type, text } = args
+
+      let keys = []
+
+      switch (type) {
+        case 'PRODUCT':
+          keys = ['products.name']
+        break;
+        case 'BRAND':
+          keys = ['brands']
+        break;
+        case 'MERCHANT':
+          keys = ['merchant']
+        break;
+        default:
+          keys = ['products.name','brands', 'merchant']
+        break
+      }
+
+      const options = {
+        includeScore: true,
+        keys
+      }
+
+      const fuse = new Fuse(merchants, options)
+      const results = fuse.search(text)
+      const flatResults = results.flatMap((result) => result.item)
+
+      return flatResults
+    }
   },
 };
 
