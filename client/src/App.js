1dd984d0ad809a0ac8c27a7ace56872c5fe8c63a
Nwaokocha Victor <nvonweb@outlook.com>
2020-12-30 10:27:29 -0500

    pass searching props to product component listen for change events in search criteria

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,47 @@ import Products from './components/Products';
 import SearchBar from "./components/SearchBar";
 
 class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+        isSearching: false,
+        searchText: '',
+        searchType: ''
+    }
+  }
+
+  componentDidMount() {
+      window.addEventListener('searchChangeEvent', this.handleSearchChange.bind(this))
+  }
+
+  handleSearchChange(e) {
+      const type = e.detail.type
+      const text = (e.detail.text || '')
+
+      if (['ALL', 'BRAND', 'PRODUCT', 'MERCHANT'].includes(type) && text.length > 3) {
+          this.setState({
+              searchText: text,
+              searchType: type,
+              isSearching: true
+          })
+      } else {
+          this.setState({
+              isSearching: false
+          })
+      }
+  }
+
   render() {
     return (
       <ApolloProvider client={apolloClient}>
         <SearchBar />
-        <Products />
+        <Products
+            isSearching={this.state.isSearching}
+            searchText={this.state.searchText}
+            searchType={this.state.searchType}
+        />
       </ApolloProvider>
     )
   }
