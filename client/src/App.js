a67b944c5cda092afb36ad4093880070387bdc51
Nwaokocha Victor <nvonweb@outlook.com>
2020-12-30 08:40:57 -0500

    Use search bar component

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react'
 import { ApolloProvider } from 'react-apollo';
 import apolloClient from './createApolloClient';
 import Products from './components/Products';
+import SearchBar from "./components/SearchBar";
 
 class App extends Component {
   render() {
     return (
       <ApolloProvider client={apolloClient}>
+        <SearchBar />
         <Products />
       </ApolloProvider>
     )
