c47559544aec2c375fa594f23b07464075fc1e08
Nwaokocha Victor <nvonweb@outlook.com>
2020-12-30 20:12:22 -0500

    display search results

--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -5,7 +5,7 @@ import { gql } from 'apollo-boost';
 import { Query } from 'react-apollo';
 import './styles.css';
 
-const GET_PRODUCTS = gql`
+export const GET_PRODUCTS = gql`
   {
     merchants {
       guid
@@ -23,7 +23,7 @@ const GET_PRODUCTS = gql`
   }
 `;
 
-const SEARCH_PRODUCTS = gql`
+export const SEARCH_PRODUCTS = gql`
   query($type: SearchType!, $text: String!) {
     search(type: $type, text: $text) {
       guid
@@ -71,26 +71,30 @@ class ProductsList extends Component {
       const { merchants, merchantsLoading } = this.props;
 
       if (!merchantsLoading && merchants && merchants.length > 0) {
-        return merchants.map(({products}) => {
-          return products && products.length > 0 && products.map(product => {
-            const { color, description, image, name, price, size } = product
-            return (
-              <Media key={product.id} className="product-card">
-              <Media left href="#">
-                <Media object src={image} alt="Product image cap" />
-                </Media>
-                <CardBody>
-                  <CardTitle style={{fontWeight: 600}}>{name}</CardTitle>
-                  <CardTitle>Price: {price}</CardTitle>
-                  <CardSubtitle>Color: {color}</CardSubtitle>
-                  <CardSubtitle>Size: {size}</CardSubtitle>
-                  <CardText>Details: {description}</CardText>
-                  <Button color="primary" size="lg" block>Buy</Button>
-                </CardBody>
-              </Media>
-            );
-          })
-        });
+        return (
+            <>
+              {merchants.map(({products}) => {
+                  return products && products.length > 0 && products.map(product => {
+                    const { color, description, image, name, price, size } = product
+                    return (
+                      <Media key={product.id} className="product-card">
+                      <Media left href="#">
+                        <Media object src={image} alt="Product image cap" />
+                        </Media>
+                        <CardBody>
+                          <CardTitle style={{fontWeight: 600}}>{name}</CardTitle>
+                          <CardTitle>Price: {price}</CardTitle>
+                          <CardSubtitle>Color: {color}</CardSubtitle>
+                          <CardSubtitle>Size: {size}</CardSubtitle>
+                          <CardText>Details: {description}</CardText>
+                          <Button color="primary" size="lg" block>Buy</Button>
+                        </CardBody>
+                      </Media>
+                    );
+                  })
+                })}
+            </>
+        )
       } else {
         return (
           <div>
@@ -107,7 +111,7 @@ class ProductsList extends Component {
         </div>
       );
     }
-  }
+}
 
 ProductsList.propTypes = {
   isSearching: PropTypes.bool,
@@ -115,4 +119,4 @@ ProductsList.propTypes = {
   searchType: PropTypes.string,
 }
 
-  export default withProducts(ProductsList)
+export default withProducts(ProductsList)
