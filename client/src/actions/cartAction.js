77d49fa60caea38936dfb2842093e94af6d8cf6b
c0d1ngd0j0 <sam.gdouglas@gmail.com>
2021-01-03 17:11:05 -0500

    added new cart actions for increasing/reducing cart item

--- client/src/actions/cartAction.js
+++ client/src/actions/cartAction.js
@@ -1,8 +1,22 @@
-import { ADD_TO_CART, REMOVE_FROM_CART, EMPTY_CART } from "./types";
+import { ADD_TO_CART, REMOVE_FROM_CART, EMPTY_CART, UPDATE_QTY } from "./types";
 
 export const addToCart = (product) =>{
   return{
     type: ADD_TO_CART,
+    payload: product
+  };
+};
+
+export const incrementItemQty = (product) =>{
+  return{
+    type: UPDATE_QTY,
+    payload: product
+  };
+};
+
+export const reduceItemQty = (product) =>{
+  return{
+    type: UPDATE_QTY,
     payload: product
   };
 };
--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,3 +1,4 @@
 export const ADD_TO_CART = "ADD_TO_CART";
 export const REMOVE_FROM_CART = "REMOVE_FROM_CART";
 export const EMPTY_CART = "EMPTY_CART";
+export const UPDATE_QTY = "UPDATE_QTY";
