ea9844a2d1eff7917913126c5ea77254d526d6ce
c0d1ngd0j0 <sam.gdouglas@gmail.com>
2021-01-03 23:46:43 -0500

    add actions for to adjust product quantity already in cart

--- client/src/actions/cartAction.js
+++ client/src/actions/cartAction.js
@@ -1,4 +1,4 @@
-import { ADD_TO_CART, REMOVE_FROM_CART, EMPTY_CART, UPDATE_QTY } from "./types";
+import { ADD_TO_CART, REMOVE_FROM_CART, EMPTY_CART, INCREASE_QTY, DECREASE_QTY } from "./types";
 
 export const addToCart = (product) =>{
   return{
@@ -7,16 +7,16 @@ export const addToCart = (product) =>{
   };
 };
 
-export const incrementItemQty = (product) =>{
+export const increasetItemQty = (product) =>{
   return{
-    type: UPDATE_QTY,
+    type: INCREASE_QTY,
     payload: product
   };
 };
 
-export const reduceItemQty = (product) =>{
+export const decreaseItemQty = (product) =>{
   return{
-    type: UPDATE_QTY,
+    type: DECREASE_QTY,
     payload: product
   };
 };
--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -2,3 +2,5 @@ export const ADD_TO_CART = "ADD_TO_CART";
 export const REMOVE_FROM_CART = "REMOVE_FROM_CART";
 export const EMPTY_CART = "EMPTY_CART";
 export const UPDATE_QTY = "UPDATE_QTY";
+export const INCREASE_QTY = "INCREASE_QTY";
+export const DECREASE_QTY = "DECREASE_QTY";
