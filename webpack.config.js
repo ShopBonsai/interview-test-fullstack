725b2e1f8e7425d172dd4c51567331e968f8afae
c0d1ngd0j0 <sam.gdouglas@gmail.com>
2021-01-03 23:48:28 -0500

    config updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 const htmlPlugin = new HtmlWebPackPlugin({
@@ -6,6 +7,11 @@ const htmlPlugin = new HtmlWebPackPlugin({
 
 module.exports = {
   entry: './client/src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js',
+    publicPath: '/'
+  },
   module: {
     rules: [
       {
