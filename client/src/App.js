018f35cb259c1336094694a5c32936a39b285de7
c0d1ngd0j0 <sam.gdouglas@gmail.com>
2021-01-03 23:49:12 -0500

    routes added, styles added for cart items quantity btns

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import { ApolloProvider } from 'react-apollo';
 import apolloClient from './createApolloClient';
-import Store from './pages/Store';
 import Cart from './pages/Cart';
 import NotFoundPage from './components/404';
 import { Provider } from "react-redux";
+import ProductsList from './components/Products';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import store from "./store";
+import NavigationBar from './components/NavBar';
 import './styles.css';
 
 class App extends Component {
@@ -15,9 +16,12 @@ class App extends Component {
       <ApolloProvider client={apolloClient}>
         <Provider store={store}>
           <Router>
+            <NavigationBar />
             <Switch>
-              <Route exact path="/" component={Store} />
-              <Route exact path="/cart" component={Cart} />
+              <Route exact path="/" render={(props) => (<ProductsList {...props}/>)} />
+              <Route path="/cart">
+                <Cart />
+              </Route>
               <Route component={NotFoundPage} />
             </Switch>
           </Router>
