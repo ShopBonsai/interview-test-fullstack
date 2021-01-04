08ef9dd6f8d2d3b889baecbfceb20c1025a022f7
c0d1ngd0j0 <sam.gdouglas@gmail.com>
2021-01-03 23:47:18 -0500

    cart reducer refactored

--- client/src/reducers/cartReducer.js
+++ client/src/reducers/cartReducer.js
@@ -1,40 +1,70 @@
-import { ADD_TO_CART, REMOVE_FROM_CART, EMPTY_CART } from "../actions/types";
+import { ADD_TO_CART, REMOVE_FROM_CART, EMPTY_CART, INCREASE_QTY, DECREASE_QTY } from "../actions/types";
 
 const initialState = {
-  items: []
+  items: {}
 };
 
 const updateCart = (items, newItem) =>{
-  const ismatch = items.find((product) => product._id === newItem._id);
-  if(ismatch){
-    return items.map((product) =>{
-      return product._id === newItem._id ? {
-        ...product, qty: product.qty + 1
-      } : product
-    });
+  let newData;
+  if(items[newItem.id]){
+    newData = {
+      ...items[newItem.id],
+      qty: items[newItem.id].qty + 1
+    }
+  } else {
+    newData = {
+      ...newItem,
+      qty: 1
+    };
   };
 
-  return [...items, {...newItem, qty: 1}]
+  return newData;
 };
 
 export default function (state = initialState, action) {
   const { type, payload } = action;
   switch (type) {
     case ADD_TO_CART:
+      let newitem = updateCart(state.items, payload);
       return{
-        ...state,
-        items: updateCart(state.items, payload)
+        items: {
+          ...state.items,
+          [newitem.id]: newitem
+        }
       };
     case REMOVE_FROM_CART:
+      delete state.items[payload.id];
       return{
         ...state,
-        items: state.items.filter((product) => product._id !== payload._id)
+        items: {
+          ...state.items
+        }
       };
     case EMPTY_CART:
       return {
         ...state,
-        items: []
-      }
+        items: {}
+      };
+    case INCREASE_QTY:
+      let updatedData = updateCart(state.items, payload);
+      return{
+        ...state,
+        items: {
+          ...state.item,
+          [updatedData.id]: updatedData
+        }
+      };
+    case DECREASE_QTY:
+      return{
+        ...state,
+        items: {
+          ...state.items,
+          [payload.id]: {
+            ...state.items[payload.id],
+            qty: state.items[payload.id].qty > 0 ? state.items[payload.id].qty - 1 : 0
+          }
+        }
+      };
     default:
       return state;
   };
