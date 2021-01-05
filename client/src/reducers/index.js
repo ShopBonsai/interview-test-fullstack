8bc2897e505f79fa59b51526e0ea978fa04e9203
c0d1ngd0j0 <sam.gdouglas@gmail.com>
2021-01-05 15:36:23 -0500

    added notification reducer to combineReducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,8 @@
 import {combineReducers} from 'redux';
 import cart from "./cartReducer";
+import notification from "./notificationReducer";
 
 export default combineReducers({
   cart,
+  notification,
 });
