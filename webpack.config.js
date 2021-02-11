1a2035f31119db9c2f28c9b4b38cbbfd7a7257ec
casserni <nicholascassera@gmail.com>
2021-02-11 14:26:19 -0500

    feat: add font awesome and images to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,12 @@ module.exports = {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.(jpg|png)$/,
+        use: {
+          loader: "url-loader",
+        },
+      },
     ],
   },
   plugins: [htmlPlugin],
