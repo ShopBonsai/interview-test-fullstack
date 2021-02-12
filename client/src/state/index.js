ddc08c42276068e70f789430ff4de0733dc9956c
casserni <nicholascassera@gmail.com>
2021-02-11 19:03:43 -0500

    feat: add context provider for state

---
+++ client/src/state/index.js
@@ -0,0 +1,32 @@
+import * as React from "react";
+
+import { initialState, hydrateState } from "./state";
+import { reducer } from "./reducer";
+import { useReactions } from "./reactions";
+
+// Here is a home brew state management that leverages React Context and reducers
+// functions similar to Redux using actions with less boiler plate
+
+const Context = React.createContext({
+  state: initialState,
+  dispatch: () => initialState,
+});
+
+export const Provider = ({ children, apolloClient }) => {
+  const [state, setState] = React.useState(hydrateState(apolloClient));
+
+  // this custom dispatch allows us to use async function inside of our reducer
+  const dispatch = async (action) => {
+    const result = await reducer(state, action);
+    setState(result);
+  };
+
+  return (
+    <Context.Provider value={{ state, dispatch }}>{children}</Context.Provider>
+  );
+};
+
+export function useState() {
+  const ctx = React.useContext(Context);
+  return [ctx.state, ctx.dispatch];
+}
