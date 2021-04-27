815ef407c330701185304e410f1579e1adfd2da5
Dilraj Somel <dsomel21@gmail.com>
2021-04-26 20:09:52 -0400

    bons2 - Not enjoying typeorm at all. Going to use Knex

---
+++ knexfile.js
@@ -0,0 +1,29 @@
+module.exports = {
+  development: {
+    client: 'mysql',
+    connection: {
+      host: 'localhost',
+      user: 'root',
+      password: 'root',
+      database: 'bonsai_demo',
+    },
+    migrations: {
+      directory: __dirname + '/server/db/migrations',
+    },
+  },
+  // production: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user: 'username',
+  //     password: 'password',
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10,
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations',
+  //   },
+  // },
+};
--- server/db/connect.js
+++ server/db/connect.js
@@ -1,29 +1,9 @@
-const { createConnection, Connection } = require('typeorm');
+const knex = require('knex');
+const knexConfig = require('../../knexfile');
 
-const DB_NAME = 'bonsai_demo';
+const environment = process.env.NODE_ENV || 'development';
+const connectionConfig = knexConfig[environment];
 
-const dbOptions = {
-  type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'root',
-  password: 'root',
-};
+const db = knex(connectionConfig);
 
-async function initializeDatabase() {
-  const connection = await createConnection(dbOptions);
-  connection.query(`CREATE DATABASE IF NOT EXISTS ${DB_NAME}`);
-  console.log('Database is ready');
-}
-
-async function connectToDatabase() {
-  const connection = createConnection({ ...dbOptions, database: DB_NAME })
-    .then(() => console.log('Database Connection Successful.'))
-    .error(err => console.log(`Could not connect to DB: ${err}`));
-}
-
-module.exports = { connectToDatabase, initializeDatabase };
-
-// 🏃‍♀️ `node server/db/connect.js`
-// initializeDatabase();
-initializeDatabase();
+module.exports = db;
