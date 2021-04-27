c8e51f4860915210e65e5fa2b893c5329625852b
Dilraj Somel <dsomel21@gmail.com>
2021-04-27 19:07:19 -0400

    bons10 Need to get this producer to behave. Will create another file 🙄

--- server/modules/Queries/Product.ts
+++ server/modules/Queries/Product.ts
@@ -1,9 +1,54 @@
-// It's called db3 because `Cannot redeclare block-scoped variable 'db'.`
+// 😠 It's called db3 because `Cannot redeclare block-scoped variable 'db'.`
 // I do not have time to fix that rn
 const db3 = require('../../db/connect');
 
 // const produceViewProductEvent = require('../../kafka/produceViewProductEvent.js');
-// const produceViewProductEvent = require('../../../server/kafka/produceViewProductEvent.js');
+
+/*----------------------------------------------------*/
+/*----------------------------------------------------*/
+/*----------------------------------------------------*/
+/*
+ * ❓ Not sure why the top isn't importing
+ * I tried using __dir as well 🤔
+ * I just copied it here so I can move on
+ */
+const { Kafka } = require('kafkajs');
+
+const config = {
+  KAFKA_HOST: 'localhost:9092',
+  KAFKA_TOPIC_NAME: 'view-product-topic',
+  CLIENT_ID: 'bonsai_kafka',
+};
+
+const kafka = new Kafka({
+  clientId: config.CLIENT_ID,
+  brokers: [config.KAFKA_HOST],
+});
+
+const producer = kafka.producer();
+
+// we define an async function that writes a new message each second
+const produceViewProductEvent = async messageObj => {
+  await producer.connect();
+
+  // after the produce has connected, we start an interval timer
+  // setInterval(async () => {
+  try {
+    // send a message to the configured topic with
+    // the key and value formed from the current value of `i`
+    await producer.send({
+      topic,
+      messages: [messageObj],
+    });
+
+    console.log(`Message has been written: ${message}`);
+  } catch (err) {
+    console.error(`Error publishing message: ${err}`);
+  }
+};
+/*----------------------------------------------------*/
+/*----------------------------------------------------*/
+/*----------------------------------------------------*/
 
 interface argsFindProduct {
   id: number;
