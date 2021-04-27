03e5e53e20ee5a011e41e1fac27e3be9e387fbc2
Dilraj Somel <dsomel21@gmail.com>
2021-04-27 17:15:17 -0400

    bons7.1 Mutation for add to cart is decent shape. Gonna have to move on tho

--- server/modules/Mutations/UserCart.ts
+++ server/modules/Mutations/UserCart.ts
@@ -5,30 +5,50 @@ interface argsAddToCart {
   quantity: number;
 }
 
-const ADD_TO_CART = async (_: any, args: argsAddToCart) => {
-  console.log('----------------------------------');
-  console.log(_, args);
+interface argsInsertUserCartProducts {
+  user_cart_id: number;
+  product_id: number;
+  quantity: number;
+}
 
+const ADD_TO_CART = async (_: any, args: argsAddToCart) => {
   // GET the current users cart
   const userCartIdRes = await db('user_carts')
     .where('user_id', args.user_id)
     .first();
 
-  console.log(userCartIdRes);
-
   const { user_id, product_id, quantity } = args;
 
-  db('user_cart_products')
-    .where('user_cart_id', userCartIdRes.id)
-    .then(async (userCart: any) => {
-      if (userCart.length === 0) {
-        await db('user_cart_products').insert({
-          user_cart_id: userCartIdRes.id,
-          product_id,
-          quantity,
-        });
+  if (db('products').where('product_id', product_id).first().quantity === 0) {
+    throw new Error('There item is currently being restocked.');
+  }
+  const insertParams: argsInsertUserCartProducts = {
+    user_cart_id: userCartIdRes.id,
+    product_id,
+    quantity,
+  };
+
+  const userCartProds = db('user_cart_products').where(
+    'user_cart_id',
+    userCartIdRes.id
+  );
+
+  userCartProds.then(async (userCart: any) => {
+    if (userCart.length === 0) {
+      await db('user_cart_products').insert(insertParams);
+    } else {
+      const dupItem = await userCartProds
+        .where('product_id', product_id)
+        .first();
+      if (dupItem) {
+        await db('user_cart_products')
+          .where('id', dupItem.id)
+          .update('quantity', dupItem.quantity + quantity);
+      } else {
+        await db('user_cart_products').insert(insertParams);
       }
-    });
+    }
+  });
 
   // Exactly the same, but TypeScript complains if I call GET_ALL_PRODUCTS_IN_CART
   const userCart = await db('user_cart_products').where(
