0bc8346ab92d2b3251561fea285597ae7cec6a4e
Dilraj Somel <dsomel21@gmail.com>
2021-04-27 14:09:13 -0400

    bons8 - Had to redo the changes on frontend to refelct new schema shape

--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
-import { CardTitle, CardSubtitle, CardText, Button, CardBody, Media } from 'reactstrap';
+import {
+  CardTitle,
+  CardSubtitle,
+  CardText,
+  Button,
+  CardBody,
+  Media,
+} from 'reactstrap';
 import { gql } from 'apollo-boost';
 import { Query } from 'react-apollo';
 import './styles.css';
@@ -8,15 +15,15 @@ const GET_PRODUCTS = gql`
   {
     merchants {
       guid
-      merchant
+      merchant_name
       products {
         id
-        name
+        product_name
         price
         description
         color
         size
-        image
+        image_url
       }
     }
   }
@@ -27,7 +34,11 @@ const withProducts = Component => props => {
     <Query query={GET_PRODUCTS}>
       {({ loading, data }) => {
         return (
-          <Component merchantsLoading={loading} merchants={data && data.merchants} {...props} />
+          <Component
+            merchantsLoading={loading}
+            merchants={data && data.merchants}
+            {...props}
+          />
         );
       }}
     </Query>
@@ -35,46 +46,56 @@ const withProducts = Component => props => {
 };
 
 class ProductsList extends Component {
-  
-    showProducts() {
-      const { merchants, merchantsLoading } = this.props;
-  
-      if (!merchantsLoading && merchants && merchants.length > 0) {
-        return merchants.map(({products}) => {
-          return products && products.length > 0 && products.map(product => {
-            const { color, description, image, name, price, size } = product
+  showProducts() {
+    const { merchants, merchantsLoading } = this.props;
+
+    if (!merchantsLoading && merchants && merchants.length > 0) {
+      return merchants.map(({ products }) => {
+        return (
+          products &&
+          products.length > 0 &&
+          products.map(product => {
+            const {
+              color,
+              description,
+              image_url,
+              product_name,
+              price,
+              size,
+            } = product;
             return (
               <Media key={product.id} className="product-card">
-              <Media left href="#">
-                <Media object src={image} alt="Product image cap" />
+                <Media left href="#">
+                  <Media object src={image_url} alt="Product image cap" />
                 </Media>
                 <CardBody>
-                  <CardTitle style={{fontWeight: 600}}>{name}</CardTitle>
+                  <CardTitle style={{ fontWeight: 600 }}>
+                    {product_name}
+                  </CardTitle>
                   <CardTitle>Price: {price}</CardTitle>
                   <CardSubtitle>Color: {color}</CardSubtitle>
                   <CardSubtitle>Size: {size}</CardSubtitle>
                   <CardText>Details: {description}</CardText>
-                  <Button color="primary" size="lg" block>Buy</Button>
+                  <Button color="primary" size="lg" block>
+                    Buy
+                  </Button>
                 </CardBody>
               </Media>
             );
           })
-        });
-      } else {
-        return (
-          <div>
-            <h3>No products available</h3>
-          </div>
         );
-      }
-    }
-  
-    render() {
+      });
+    } else {
       return (
         <div>
-          {this.showProducts()}
+          <h3>No products available</h3>
         </div>
       );
     }
   }
-  export default withProducts(ProductsList)
+
+  render() {
+    return <div>{this.showProducts()}</div>;
+  }
+}
+export default withProducts(ProductsList);
