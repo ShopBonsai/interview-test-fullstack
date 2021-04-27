99eaa418e646de22624fa2284857e60bd323e5e4
Dilraj Somel <dsomel21@gmail.com>
2021-04-26 20:32:50 -0400

    bons4 - Created migration for `products`, but need `brands` table first.

--- server/db/connect.js
+++ server/db/connect.js
@@ -6,4 +6,8 @@ const connectionConfig = knexConfig[environment];
 
 const db = knex(connectionConfig);
 
+/*
+ * TODO: Create a init() script to CREATE DATABASE IF NOT EXISTS
+ * For now, it is a matter of going into mysql cli `mysql -u root -p` and creating DB
+ */
 module.exports = db;
--- server/db/migrations/20210427001613_CreateProductsTable.js
+++ server/db/migrations/20210427001613_CreateProductsTable.js
@@ -1,8 +1,32 @@
+exports.up = async function startMigration(knex) {
+  await knex.schema.createTable('products', function createProductsTable(t) {
+    t.increments().notNullable();
 
-exports.up = function(knex) {
-  
+    // belongs_to a `brand`
+    t.integer('brand_id')
+      .unsigned()
+      .references('id')
+      .inTable('brands')
+      .onDelete('cascade');
+
+    t.string('product_name').notNullable();
+    t.specificType('price', 'double precision').notNullable(); // 👉 More accuracy
+    t.text('description', 'mediumtext');
+    t.string('color');
+    t.string('size');
+    t.integer('quantity').notNullable(); // 🤔 May want to put this in another table
+    t.string('image_url');
+
+    // TIMESTAMPS
+    t.dateTime('created_at')
+      .notNullable()
+      .defaultTo(knex.raw('CURRENT_TIMESTAMP'));
+    t.dateTime('updated_at').defaultTo(
+      knex.raw('NULL ON UPDATE CURRENT_TIMESTAMP')
+    );
+  });
 };
 
-exports.down = function(knex) {
-  
+exports.down = async function startMigration(knex) {
+  await knex.schema.dropTableIfExists('products');
 };
