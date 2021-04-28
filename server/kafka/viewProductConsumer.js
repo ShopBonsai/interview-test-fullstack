6f59fda982a56f316eedd7b062f78c9fcf4a944c
Dilraj Somel <dsomel21@gmail.com>
2021-04-27 20:52:55 -0400

    bons10 Created a "cache" class that we can act when messages get produced

--- server/kafka/viewProductConsumer.js
+++ server/kafka/viewProductConsumer.js
@@ -19,11 +19,53 @@ const consumeViewProductEvents = async () => {
   await consumer.subscribe({ topic: config.KAFKA_TOPIC_NAME });
   await consumer.run({
     eachMessage: ({ message }) => {
-      // here, we just log the message to the standard output
-      console.log(`received message: ${message.value}`);
+      handleProductViewEvent(JSON.parse(message.value));
     },
   });
 };
+
+class ProductViewCache {
+  constructor() {
+    this.cacheObj = {};
+  }
+
+  hasUser(userId) {
+    return userId in this.cacheObj;
+  }
+
+  has(userId, productId) {
+    if (this.hasUser(userId) === false) return false;
+    return productId in this.cacheObj[userId];
+  }
+
+  incrementViewFor(userId, productId) {
+    if (!this.hasUser(userId)) this.cacheObj[userId] = {};
+    if (!this.has(userId, productId)) {
+      this.cacheObj[userId][productId] = { views: 0 };
+    }
+
+    ++this.cacheObj[userId][productId].views;
+  }
+
+  resetViewsFor(userId, productId) {
+    if (this.has(userId, productId)) {
+      this.cacheObj[userId][productId] = { views: 0 };
+    }
+  }
+
+  getNumViewsFor(userId, productId) {
+    if (this.has(userId, productId) === false) return null;
+    return this.cacheObj[userId][productId].views;
+  }
+}
+
+function handleProductViewEvent(eventBlob) {
+  console.log('Incoming Event: ', eventBlob);
+  const productId = eventBlob.id;
+  const userId = eventBlob.user_id;
+
+  // State Machine
+}
 
 // Turns on consumer
 consumeViewProductEvents();
