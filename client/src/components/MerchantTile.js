f8ae806e80849d3798dec5c4a39b4b35365eb7e9
tf-anon <tomfell2@hotmail.com>
2021-05-08 15:44:11 -0400

    Separate products into components

---
+++ client/src/components/MerchantTile.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Container, Row, Col, CardTitle, CardText, Media } from 'reactstrap';
+
+import { ProductCard } from './ProductCard';
+
+const styles = {
+    container: {
+        boxShadow: "5px 5px 10px rgb(136 10 142 / 40%)",
+        borderRadius: "10px",
+        margin: "1rem",
+        width: "95%"
+    },
+    left: {
+        padding: "2rem"
+    },
+};
+
+export const MerchantTile = ({data}) => {
+    const { merchant, companyDescription, logo, publishedState, products} = data;
+
+    if (!publishedState) {
+        // we only want to render a given merchant if they are published
+        return null;
+    }
+
+    return <>
+        <Container style={styles.container} fluid="true">
+           <Row style={styles.row}>
+                <Col style={styles.left} sm="4">
+                    <Row>
+                        <Media object src={logo} alt=" Merchant logo" className="icon" />
+                        <CardTitle className="text-title">{merchant}</CardTitle>
+                    </Row>
+                    <CardText>About {merchant}: {companyDescription}</CardText>
+                </Col>
+                <Col sm="7" >
+                    {products && products.length > 0 && products.map((product) => <ProductCard key={product.id} data={product} />)} 
+                </Col>
+           </Row>     
+      </Container>
+    </>
+}
---
+++ client/src/components/ProductCard.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { CardTitle, CardSubtitle, CardText, Button, CardBody, Media } from 'reactstrap';
+
+export const ProductCard = ({data}) => {
+    const { color, description, image, name, price, quantity, size } = data;
+                        
+    return (
+        <Media className="product-card">
+            <Media left href="#">
+                <Media object src={image} alt="Product image cap" className="img-tile" />
+            </Media>
+            <CardBody>
+                <CardTitle className="text-title">{name}</CardTitle>
+                <CardTitle>Price: ${(price / 100).toFixed(2)}</CardTitle>
+                <CardSubtitle>Color: {color}</CardSubtitle>
+                <CardSubtitle>Size: {size}</CardSubtitle>
+                <CardSubtitle>Quantity: {quantity}</CardSubtitle>
+                <CardText>Details: {description}</CardText>
+                <Button color="primary" size="lg" block>Buy</Button>
+            </CardBody>
+        </Media>
+        );
+}
