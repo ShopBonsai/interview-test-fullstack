3e84534cae1ada484ee4bcc25e4a440231d04f6d
Matheus Ferreira <mferreira@teamliquid.com>
2021-10-03 15:39:03 -0300

    Including entire data

--- server/utils/seed.data.ts
+++ server/utils/seed.data.ts
@@ -1,182 +1,6 @@
 import { Merchant, MerchantModel } from './../modules/entities/merchant';
 import faker from 'faker';
 
-const tempMerchants = [
-  {
-    index: 0,
-    guid: '8b36dad9-f5a5-4b35-bfc9-27990a0be2b2',
-    logo: 'https://www.fillmurray.com/50/50',
-    dateCreated: '2016-04-22T06:25:51 +04:00',
-    publishedState: false,
-    brands: ['Pansy Espinoza', 'Finley Barron', 'Chavez Pope'],
-    products: [
-      {
-        belongsToBrand: 2,
-        id: '37b4d1b7-ed3c-4d28-91e0-ef0fb89b2b12',
-        name: 'VENIAM Swimming Shorts',
-        price: 706.2,
-        description: 'Excepteur non Lorem aliqua esse irure veniam elit labore culpa velit nostrud ad nostrud.',
-        color: 'deserunt',
-        size: 'L',
-        quantity: 1,
-        image: 'https://picsum.photos/300/?random',
-      },
-      {
-        belongsToBrand: 0,
-        id: '6e73c7a1-d59f-48d3-b547-ca7504ed581d',
-        name: 'ANIM Underwear',
-        price: 516.2,
-        description: 'Nostrud fugiat duis ea duis laborum mollit eiusmod sunt nisi sunt pariatur nostrud et.',
-        color: 'tempor',
-        size: 'L',
-        quantity: 4,
-        image: 'https://picsum.photos/300/?random',
-      },
-      {
-        belongsToBrand: 0,
-        id: '3e0aa76a-8236-49e6-80ec-0bfa62dce56b',
-        name: 'SIT Polo Shirt',
-        price: 523.4,
-        description: 'Laborum non sint nostrud quis minim consequat labore sit dolor est sit.',
-        color: 'velit',
-        size: 'M',
-        quantity: 2,
-        image: 'https://picsum.photos/300/?random',
-      },
-      {
-        belongsToBrand: 1,
-        id: 'f569166f-2827-4f10-8c91-47ade5b11a57',
-        name: 'CUPIDATAT Sandals',
-        price: 992.4,
-        description: 'Elit dolor aliqua culpa ea labore ullamco ea aliquip cupidatat eu dolore.',
-        color: 'pariatur',
-        size: 'S',
-        quantity: 9,
-        image: 'https://picsum.photos/300/?random',
-      },
-      {
-        belongsToBrand: 0,
-        id: '682d3464-4ae0-4f4b-b26a-e1e42fdc7ff2',
-        name: 'ET Poncho',
-        price: 336.8,
-        description: 'Duis dolor eu in ea quis et exercitation dolor officia.',
-        color: 'aute',
-        size: 'M',
-        quantity: 4,
-        image: 'https://picsum.photos/300/?random',
-      },
-    ],
-    merchant: 'CORPULSE',
-    commissionFee: '17%',
-    contactEmail: 'chavezpope@corpulse.com',
-    phone: '+1 (976) 449-2415',
-    address: '339 Stewart Street, Boykin, Texas, 5841',
-    publishedDate: '2017-03-19T06:55:52 +04:00',
-    publishedBy: {
-      userId: '1667143e-bf62-43be-8fce-12d739d6604f',
-    },
-    companyDescription:
-      'Nisi aliqua culpa veniam veniam aliquip amet pariatur esse eiusmod sint. Aliqua enim ad cupidatat eu eu. Cillum aute consectetur dolore nisi excepteur ullamco ad velit magna consequat ad duis Lorem aliqua. Commodo deserunt et nostrud veniam deserunt consequat excepteur Lorem. Pariatur aliquip aliquip fugiat quis mollit enim cupidatat ex. Culpa exercitation fugiat eiusmod cillum cupidatat exercitation elit eu.\r\n',
-  },
-  {
-    index: 1,
-    guid: '91749c6f-fa4e-456a-829f-22aaebec15f7',
-    logo: 'https://www.fillmurray.com/50/50',
-    dateCreated: '2015-10-22T06:49:34 +04:00',
-    publishedState: false,
-    brands: ['Simone Gallagher', 'Christian Short', 'Elisa Ellis'],
-    products: [
-      {
-        belongsToBrand: 2,
-        id: 'c1ea8941-f81d-40d8-87f2-c77ad89a809b',
-        name: 'TEMPOR Sunglasses',
-        price: 220.7,
-        description: 'Ex incididunt velit elit culpa excepteur amet cillum amet in occaecat laboris quis.',
-        color: 'non',
-        size: 'M',
-        quantity: 1,
-        image: 'https://picsum.photos/300/?random',
-      },
-      {
-        belongsToBrand: 1,
-        id: '8704371c-84f5-4c88-a2ba-7b0a978a7cc2',
-        name: 'QUIS Boots',
-        price: 550.8,
-        description: 'Cupidatat voluptate veniam in commodo elit.',
-        color: 'elit',
-        size: 'M',
-        quantity: 3,
-        image: 'https://picsum.photos/300/?random',
-      },
-      {
-        belongsToBrand: 1,
-        id: '5417c38f-9162-4f91-bfba-da793871627c',
-        name: 'VENIAM Fleece',
-        price: 498,
-        description: 'Adipisicing aute mollit irure ea cupidatat.',
-        color: 'eiusmod',
-        size: 'S',
-        quantity: 5,
-        image: 'https://picsum.photos/300/?random',
-      },
-    ],
-    merchant: 'ORBIXTAR',
-    commissionFee: '13%',
-    contactEmail: 'elisaellis@orbixtar.com',
-    phone: '+1 (907) 429-3484',
-    address: '615 Stryker Court, Elliott, Florida, 3053',
-    publishedDate: '2017-04-24T03:25:49 +04:00',
-    publishedBy: {
-      userId: 'f7c0b453-47fd-45bb-a40b-1af76a1a9ba0',
-    },
-    companyDescription:
-      'Occaecat ullamco id et velit. Ea tempor occaecat aliquip enim duis eiusmod aute aute commodo deserunt Lorem dolor amet. Deserunt mollit qui ea adipisicing deserunt duis exercitation excepteur commodo.\r\n',
-  },
-  {
-    index: 2,
-    guid: '2e0709f5-0b43-45c6-a0b5-13df403ca19f',
-    logo: 'https://www.fillmurray.com/50/50',
-    dateCreated: '2017-06-22T10:35:12 +04:00',
-    publishedState: true,
-    brands: ['Goldie Lynch', 'Miller Bird', 'Melanie Conner'],
-    products: [
-      {
-        belongsToBrand: 0,
-        id: '57aa9c28-a38c-4488-affc-4dbda0356314',
-        name: 'DOLOR Pajamas',
-        price: 305.7,
-        description: 'Ex nulla officia pariatur nostrud nisi dolore esse quis.',
-        color: 'enim',
-        size: 'L',
-        quantity: 5,
-        image: 'https://picsum.photos/300/?random',
-      },
-      {
-        belongsToBrand: 2,
-        id: '5b9f9ab7-52c8-4d96-b1de-2dfbc7f38304',
-        name: 'MOLLIT Sunglasses',
-        price: 578,
-        description: 'Eiusmod tempor laborum reprehenderit adipisicing consequat elit aute laborum quis adipisicing.',
-        color: 'fugiat',
-        size: 'S',
-        quantity: 3,
-        image: 'https://picsum.photos/300/?random',
-      },
-    ],
-    merchant: 'INSURITY',
-    commissionFee: '17%',
-    contactEmail: 'melanieconner@insurity.com',
-    phone: '+1 (903) 580-3503',
-    address: '880 Java Street, Shelby, Michigan, 9455',
-    publishedDate: '2017-02-10T05:01:15 +05:00',
-    publishedBy: {
-      userId: '3f88b1da-d453-4a9f-9b02-a3022a31bc58',
-    },
-    companyDescription:
-      'Dolore reprehenderit enim proident ut in mollit. In non nostrud cillum deserunt incididunt. Qui exercitation occaecat esse ipsum aliquip laboris irure et sit elit veniam veniam Lorem. Lorem occaecat adipisicing minim Lorem dolore duis tempor commodo fugiat quis ipsum amet. Aliquip consectetur ex mollit nisi adipisicing proident ad ea consequat dolore minim consequat tempor.\r\n',
-  },
-];
-
 const merchants = [
   {
     index: 0,
@@ -5369,15 +5193,15 @@ const merchants = [
 ];
 
 export async function seedDatabase() {
-  const merchants = await MerchantModel.find({}).estimatedDocumentCount();
+  const data = await MerchantModel.find({}).estimatedDocumentCount();
 
-  if (merchants > 0) {
+  if (data > 0) {
     console.info('DB already populated');
     return;
   }
 
   await Promise.all(
-    tempMerchants.map(async (merchant) => {
+    merchants.map(async (merchant) => {
       const entity = new MerchantModel(merchant);
 
       entity.publishedBy.email = faker.internet.email();
